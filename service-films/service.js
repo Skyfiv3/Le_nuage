e5189a186ab983283437045b1c8d5a3c937fa6ff
SophieNo <145771471+SophieNo@users.noreply.github.com>
2025-11-25 15:21:09 +0000

    Add function filter film 2025

--- service-films/service.js
+++ service-films/service.js
@@ -17,4 +17,20 @@ app.get("/films", async (req, res) => {
   res.json(films);
 });
 
+app.get("/films/recents", async (req, res) => {
+  const response = await fetch(`https://j55wpg0mc7.execute-api.eu-west-2.amazonaws.com/default/Get_Movies`);
+  const data = await response.json();
+
+  const films2025 = data.results.filter(film => film.year = '2025'); 
+
+  const films = films2025.results.map(film => ({
+    id: film.id,
+    name: film.name,
+    description: film.description,
+    year: film.year,
+  }));
+
+  res.json(films);
+});
+
 app.listen(4000, () => console.log("Service Films sur http://localhost:4000"));
